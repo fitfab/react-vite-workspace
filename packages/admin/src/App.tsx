6f1144e2804a83e8a7b6d4b9d9c6f7aa7d6f1afa
Miguel Julio <fitfab@yahoo.com>
2023-03-04 16:27:58 -0500

    feat: add husky and lin-staged

--- packages/admin/src/App.tsx
+++ packages/admin/src/App.tsx
@@ -5,7 +5,7 @@ import reactLogo from './assets/react.svg';
 
 function App() {
   const [count, setCount] = useState(0);
-  const foo = 'bar';
+  const foo = 'baronn';
   console.count('render');
   return (
     <div className="App">
