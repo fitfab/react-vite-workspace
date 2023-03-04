dd9410c774b651d519911f69bc2bfeb4f474117d
Miguel Julio <fitfab@yahoo.com>
2023-03-04 15:54:55 -0500

    feat: add eslint prettier

--- packages/admin/src/App.tsx
+++ packages/admin/src/App.tsx
@@ -1,33 +1,30 @@
-import { Button } from "@fitfab/uix";
-import { useState } from "react";
-import "./App.css";
-import reactLogo from "./assets/react.svg";
+import { Button } from '@fitfab/uix';
+import { useState } from 'react';
+import './App.css';
+import reactLogo from './assets/react.svg';
 
 function App() {
   const [count, setCount] = useState(0);
-  console.count("render");
+  const foo = 'bar';
+  console.count('render');
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" rel="noreferrer" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
+        <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
   );
 }
--- packages/uix/src/button/Button.tsx
+++ packages/uix/src/button/Button.tsx
@@ -1,21 +1,20 @@
-import "./button.css";
+import './button.css';
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  primary?: boolean;
 }
-export const Button = ({
-  children,
-}: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+export const Button = ({ children, primary = true }: ButtonProps) => {
+  const bar = 'foo';
+  const bgColor = primary ? 'orange' : '#70b42b';
   return (
     <button
       style={{
-        backgroundColor: "#70b42b",
-        color: "#fff",
-        border: "2px solid #ae68da",
-        borderRadius: ".5rem",
-        padding: ".5rem 1rem",
-        cursor: "pointer",
+        backgroundColor: bgColor,
+        color: '#fff',
+        border: '2px solid #ae68da',
+        borderRadius: '.5rem',
+        padding: '.5rem 1rem',
+        cursor: 'pointer',
       }}
     >
       {children}
